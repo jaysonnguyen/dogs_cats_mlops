d780c23de19fd1ad57944b89d76c0f871a6fb125
neonampv <dev01@neoscorp.vn>
2022-06-15 14:29:06 +0700

    fastapi predict

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import inference_onnx
+
+app = FastAPI(title="Dogs And Cats MLOps")
+
+@app.get('/')
+async def home():
+    return "<h2>Home"
+
+@app.get('/predict')
+async def get_prediction(image_path: str):
+    labels = ['Cat', 'Dog']
+    img = inference_onnx.processing_image(image_path)
+    result = inference_onnx.predict(img)
+    print(result)
+    return labels[result]
