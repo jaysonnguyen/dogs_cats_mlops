3530d7f72544bd9f8afd89ec8f63d07a2037d7c7
neonampv <dev01@neoscorp.vn>
2022-06-23 15:11:08 +0700

    change lambda handler

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,13 +1,23 @@
 import json
-from inference_onnx import processing_image, predict
+from inference_onnx import base64_to_image, processing_image, predict
 
 def lambda_handler(event, context):
-    b64_value = event['b64_image']
-    print(f"Got the input base64")
-    image = processing_image(b64_value)
-    response = predict(image)
-    return {
-        'statusCode': 200,
-        'headers': {},
-        'predicted': json.dumps(response)
-    }
+    if 'resource' in event.keys():
+        body = event['body']
+        body = json.loads(body)
+        print(f"Got the input base64")
+        image = processing_image(body['b64_image'])
+        response = predict(image)
+        return {
+            'statusCode': 200,
+            'headers': {},
+            'predicted': json.dumps(response)
+        }
+    else:
+        image = processing_image(event['b64_image'])
+        response = predict(image)
+        return {
+            'statusCode': 200,
+            'headers': {},
+            'predicted': json.dumps(response)
+        }
